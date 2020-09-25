b8a1692d30306d184cbc21f306ab712d10cd96ed
keshprad <keshav.r.pradeep@gmail.com>
2020-09-25 08:21:45 -0700

    moved to single file

--- src/gitBot.py
+++ src/gitBot.py
@@ -1,19 +1,39 @@
 import re
 import requests
+import discord
+from discord.ext import commands
+import random
 from bs4 import BeautifulSoup
 
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print("I'm ready!")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
+
+
+@bot.command()
+async def code(ctx, *, url):
+    api_url = find_api_url(url)
+    code = get_code(api_url)
+    await ctx.send(f"**Here's the code:** ```{code}```")
 
 
 def get_code(url):
     res = requests.get(url)
     code = res.text
-
     return code
+
 
 def find_api_url(github_url):
     exclusions = ['https://', 'github.com/', 'github.com']
     url_data = re.sub('|'.join(exclusions), '', github_url)
-
     url_data = url_data.split(sep='/')
 
     file = {"user": url_data[0], "repo": url_data[1], "branch": url_data[3], "file_path": '/'.join(url_data[4:])}
@@ -21,9 +41,5 @@ def find_api_url(github_url):
     api_url = f"https://raw.githubusercontent.com/{file['user']}/{file['repo']}/{file['branch']}/{file['file_path']}"
     return api_url
 
-if __name__ == '__main__':
-    url = "https://github.com/keshprad/Algorithms/blob/master/BellmanFordShortestPath/bellman_ford.py"
-    api_url = find_api_url(url)
-    code = get_code(api_url)
-    print(code)
-    print(api_url)
+
+bot.run(open("../bot_token", "r").read())
